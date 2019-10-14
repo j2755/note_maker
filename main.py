9ba6df6b7fd2ad815007318f9c9ecfa982e11fb8
j2766 <jjosemanuel64@gmail.com>
2019-10-13 17:24:02 -0700

    added screens and succeeded in getting input from text

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 from kivy.app import App
-class WeatherApp(App):
- pass
-if __name__ == '__main__':
- WeatherApp().run()
+from kivy.uix.widget import Widget
+from kivy.uix.textinput import TextInput 
+from kivy.uix.gridlayout import GridLayout
+from kivy.lang import Builder
+from kivy.uix.relativelayout import RelativeLayout 
+from kivy.uix.screenmanager import ScreenManager,Screen
+
+
+class Home(Screen):
+	pass
+class Work(Screen):
+	pass
+class Alternate(Screen):
+	pass
+
+class ScreenManagement(ScreenManager):
+    pass
+
+presentation = Builder.load_file("NotePad.kv")
+
+class NotePadApp(App):
+	def build(self):
+		return presentation
+	def process(self):
+		text=self.root.ids.input.text
+		print(text)
+	
+
+NotePadApp().run()
